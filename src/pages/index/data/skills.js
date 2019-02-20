0f34feb9d1e560c80c0f1d1215d04531964021c2
hujiaojiao <huwanqi1234@gmail.com>
2019-02-20 16:48:52 +0800

    huwanqi: bugfix

--- src/pages/index/data/skills.js
+++ src/pages/index/data/skills.js
@@ -2,12 +2,12 @@ const skills = [
     '有较多数据可视化项目经历，熟练使用 d3、G2、Three.js 等可视化组件库；',
     '熟练掌握 javascript，熟练运用 react、vue 等前端框架；',
     // '熟练掌握 css，能使用 ant-design、element、bootstrap 等 UI 组件库快速搭建页面；',
-    '了解 web 后端相关技能，能用 nodejs 搭建简单的 web 后端项目；',
+    '熟练掌握 web 后端相关技能，熟练运用 express, eggjs 等后端 web 框架；',
     '熟练掌握 mysql、mongodb 等数据库的使用；',
     '熟练使用 docker 部署应用；',
     // '掌握 webpack 等前端项目打包工具；',
     '熟练使用 git、svn 等版本控制系统；',
-    '英语：良好的读写能力，CET6：521；',
+    '英语：良好的读写说能力，CET6：521；',
 ];
 
 export default skills;
