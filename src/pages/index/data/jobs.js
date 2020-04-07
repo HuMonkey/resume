df80cb2509f6ad22be371f3f111444d4f51eb088
hujiaojiao <huwanqi1234@gmail.com>
2020-04-07 16:10:31 +0800

    huwanqi: update

--- src/pages/index/data/jobs.js
+++ src/pages/index/data/jobs.js
@@ -1,6 +1,7 @@
 const jobs = [
-    { company: '贝莱德香港', position: '数据可视化分析师', startTime: '2018.12', endTime: '~' },
-    { company: '新华智云科技有限公司', position: '数据可视化', startTime: '2017.04', endTime: '2018.10' },
+    { company: '微众银行', position: '数据可视化/前端开发', startTime: '2019.6', endTime: '~' },
+    { company: '贝莱德香港', position: '数据可视化/全栈开发', startTime: '2018.12', endTime: '2019。6' },
+    { company: '新华智云科技有限公司', position: '数据可视化/前端开发', startTime: '2017.04', endTime: '2018.10' },
     { company: '网易有道', position: '前端开发实习生', startTime: '2016.07', endTime: '2016.09' },
     // { company: '旅居星球网络科技有限公司', position: '创始人之一、前端开发', startTime: '2014.12', endTime: '2015.12' },
 ];
--- src/pages/index/data/skills.js
+++ src/pages/index/data/skills.js
@@ -1,8 +1,9 @@
 const skills = [
+    '有独立带领前端团队的经验，能很好地协调组内人员分工',
     '有较多数据可视化项目经历，熟练使用 d3、G2、Three.js 等可视化组件库；',
-    '熟练掌握 javascript，熟练运用 react、vue 等前端框架；',
+    '熟练掌握 javascript，熟练运用 react 等前端框架；',
     // '熟练掌握 css，能使用 ant-design、element、bootstrap 等 UI 组件库快速搭建页面；',
-    '熟练掌握 web 后端相关技能，熟练运用 express, eggjs 等后端 web 框架；',
+    '熟练掌握 web 后端相关技能，熟练运用 express, eggjs 等后端 nodejs 框架；',
     '熟练掌握 mysql、mongodb 等数据库的使用；',
     '熟练使用 docker 部署应用；',
     // '掌握 webpack 等前端项目打包工具；',
